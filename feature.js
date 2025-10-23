b92940fb21b5762328c9e52a8c59706be42040fa
phoenixixixi <mhammmaddustin@gmail.com>
2025-10-23 21:32:40 +0700

    mengubah file features

--- feature.js
+++ feature.js
@@ -1,5 +1,9 @@
 function tambah(...bilangan) {
-  return bilangan.reduce((total, num) => total + num);
+  return bilangan.reduce((total, num) => total + num)
 }
 
-console.log(tambah(9, 5));
+function kurang(...bilangan) {
+  return bilangan.reduce((total, num) => total - num)
+}
+
+console.log(tambah(9, 5))
