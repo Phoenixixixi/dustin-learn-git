8366ff97949ba90f86518bb7321c13cd0c10debf
phoenixixixi <mhammmaddustin@gmail.com>
2025-10-23 21:35:48 +0700

    mengubah isi feature

--- feature.js
+++ feature.js
@@ -1,5 +1,8 @@
 function tambah(...bilangan) {
-  return bilangan.reduce((total, num) => total + num);
+  return bilangan.reduce((total, num) => total + num)
 }
 
-console.log(tambah(9, 5));
+function kali(...bilangan) {
+  return kali.reduce((total, num) => total * num)
+}
+console.log(tambah(9, 5))
